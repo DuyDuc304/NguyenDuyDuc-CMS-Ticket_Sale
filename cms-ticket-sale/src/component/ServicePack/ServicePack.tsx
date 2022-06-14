2f431438dd0224aa3ec88d94be7d358eef5a2e33
DuyDuc304 <ducpy30042000@gmail.com>
2022-06-14 17:08:20 +0700

    t3

--- cms-ticket-sale/src/component/ServicePack/ServicePack.tsx
+++ cms-ticket-sale/src/component/ServicePack/ServicePack.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 import { Menubar } from "../layouts/menubar";
+import Nextpage from "../layouts/nextpage";
 import Topbar from "../layouts/topbar";
 
 class ServicePack extends React.Component {
@@ -10,6 +12,26 @@ class ServicePack extends React.Component {
                 <Menubar buttonid='cd' />
                 <div className="bgapp">
                     <label className="titleapp"> Danh sách gói vé</label>
+                    <div className="sreach-qlv">
+                        <form autoComplete='off' >
+                            <input className="sreach-input" name='tìm kiếm' type="text" placeholder='Tìm bằng số vé' />
+                            <button className="sreach-button"><BiSearch /></button>
+                        </form>
+                    </div>
+
+                    <div className="dsgv-row-button">
+                        <a href="/ServicePack" className="qlv-download notline">Xuất file (.csv)</a>
+                        <div className="add-ServicePack-btn">
+                            <p >Thêm gói vé</p>
+                        </div>
+
+                    </div>
+
+
+
+                    <div className="qlv-nextpage">
+                        <Nextpage />
+                    </div>
                 </div>
             </div>
         )
--- cms-ticket-sale/src/component/TicketChange/TicketChange.tsx
+++ cms-ticket-sale/src/component/TicketChange/TicketChange.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Menubar } from "../layouts/menubar";
+import Nextpage from "../layouts/nextpage";
+import Sreach from "../layouts/sreach";
 import Topbar from "../layouts/topbar";
 
 class TicketChange extends React.Component {
     render() {
         return (
             <div className='bg'>
+                <Sreach />
                 <Topbar />
                 <Menubar buttonid='dsv' />
-                <div className="bgapp">
+                <div className="bgapp-dsv1">
                     <label className="titleapp"> Đổi soát vé</label>
+
+
+
+                    <div className="dsv-nextpage"><Nextpage /></div>
+                </div>
+                <div className="bgapp-dsv2">
+
+
                 </div>
             </div>
         )
