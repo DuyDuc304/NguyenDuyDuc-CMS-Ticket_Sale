15d674f8ea1bd8b5e7ef5a283304451f40a3ac58
DuyDuc304 <ducpy30042000@gmail.com>
2022-06-14 17:16:26 +0700

    t3

--- cms-ticket-sale/src/component/TicketChange/TicketChange.tsx
+++ cms-ticket-sale/src/component/TicketChange/TicketChange.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 import { Menubar } from "../layouts/menubar";
 import Nextpage from "../layouts/nextpage";
 import Sreach from "../layouts/sreach";
@@ -13,6 +14,17 @@ class TicketChange extends React.Component {
                 <Menubar buttonid='dsv' />
                 <div className="bgapp-dsv1">
                     <label className="titleapp"> Đổi soát vé</label>
+                    <div className="sreach-qlv" style={{ top: '110px' }}>
+                        <form autoComplete='off' >
+                            <input className="sreach-input" name='tìm kiếm' type="text" placeholder='Tìm bằng số vé' />
+                            <button className="sreach-button"><BiSearch /></button>
+                        </form>
+                    </div>
+                    <div className="dsv-cds-btn">
+                        <div className="add-ServicePack-btn">
+                            <p >Thêm gói vé</p>
+                        </div>
+                    </div>
 
 
 
