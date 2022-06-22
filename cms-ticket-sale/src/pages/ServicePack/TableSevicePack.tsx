137b23f921060bbba2ede6a01a455bc55bc99462
DuyDuc304 <ducpy30042000@gmail.com>
2022-06-22 13:02:25 +0700

    update

--- cms-ticket-sale/src/pages/ServicePack/TableSevicePack.tsx
+++ cms-ticket-sale/src/pages/ServicePack/TableSevicePack.tsx
@@ -24,40 +24,42 @@ export const TableSevicePack = () => {
     function LoadData(Data: any) {
         var div: any = document.getElementById('dataServicePack')
         var html: any = Data.map(function (item: any) {
-            return (<ul className={setcolor(item.id)} key={item.id}>
-                <div className="table-sivice-park-col1" > <p>{item.STT}</p></div>
-                <div className="table-sivice-park-col2"><p>{item.TicketNumber}</p></div>
-                <div className="table-sivice-park-col3"><p>{item.NamePacket}</p></div>
-                <div className="table-sivice-park-col4">
-                    <div className="colum-date-time">
-                        <label> {item.Date} </label>
-                        <label> 08:00:00</label>
+            return (
+                <ul className={setcolor(item.id)} key={item.id}>
+                    <div className="table-sivice-park-col1" > <p>{item.STT}</p></div>
+                    <div className="table-sivice-park-col2"><p>{item.TicketNumber}</p></div>
+                    <div className="table-sivice-park-col3"><p>{item.NamePacket}</p></div>
+                    <div className="table-sivice-park-col4">
+                        <div className="colum-date-time">
+                            <label> {item.Date} </label>
+                            <label> 08:00:00</label>
+                        </div>
                     </div>
-                </div>
-                <div className="table-sivice-park-col5">
-                    <div className="colum-date-time">
-                        <label> {item.DateEnd} </label>
-                        <label> 08:00:00</label>
+                    <div className="table-sivice-park-col5">
+                        <div className="colum-date-time">
+                            <label> {item.DateEnd} </label>
+                            <label> 08:00:00</label>
+                        </div>
                     </div>
-                </div>
-                <div className="table-sivice-park-col6">
-                    <div className="number-money-ticket">
-                        <label> {item.TicketPrice} VND</label>
+                    <div className="table-sivice-park-col6">
+                        <div className="number-money-ticket">
+                            <label> {item.TicketPrice} VND</label>
 
+                        </div>
                     </div>
-                </div>
-                <div className="table-sivice-park-col7"><p>{item.TicketComboPrice} </p></div>
-                <div className="table-sivice-park-col8">
-                    {status(item.Status)}
-                </div>
-                <div className="table-sivice-park-col9">
-                    <div className="row-update" onClick={() => ShowSevicePark('updatesvp')}>
-                        <p className="icon-update" ><HiOutlinePencilAlt /></p>
-                        <p >Cập nhật</p>
+                    <div className="table-sivice-park-col7"><p>{item.TicketComboPrice} </p></div>
+                    <div className="table-sivice-park-col8">
+                        {status(item.Status)}
                     </div>
-                </div>
-            </ul>)
+                    <div className="table-sivice-park-col9">
+                        <div className="row-update" onClick={() => ShowSevicePark('updatesvp')}>
+                            <p className="icon-update" ><HiOutlinePencilAlt /></p>
+                            <p >Cập nhật</p>
+                        </div>
+                    </div>
+                </ul>)
         })
+        div.innerHTML = html.innerHTML
 
     }
 
@@ -147,7 +149,7 @@ export const TableSevicePack = () => {
             <div id="dataServicePack">
 
             </div>
-            {Data1.map((item, index) => (
+            {/* {Data1.map((item, index) => (
                 <ul className={setcolor(index)} key={item.id}>
                     < div className="table-sivice-park-col1" > <p>{item.STT}</p></div>
                     <div className="table-sivice-park-col2"><p>{item.TicketNumber}</p></div>
@@ -181,7 +183,7 @@ export const TableSevicePack = () => {
                         </div>
                     </div>
                 </ul>
-            ))}
+            ))} */}
         </div>
     )
 
